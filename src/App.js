c7ffd9672c248cdd1ac1a6e69e389bf13bdb8453
Omar <omur.bassiouni@gmail.com>
2022-01-12 06:07:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Navbar from './components/Navbar/Navbar';
+import NewsContent from './components/NewsContent/NewsContent';
+import Footer from './components/Footer/Footer';
+import Pagination from './components/Pagination/Pagination';
 
-function App() {
+
+
+export default function App() {
+
+  const [category, setCategory] = useState("sport");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Navbar setCategory={setCategory} />
+      <NewsContent category={category} />
+      <Footer />
     </div>
-  );
-}
+  )
+};
 
-export default App;
+
+
